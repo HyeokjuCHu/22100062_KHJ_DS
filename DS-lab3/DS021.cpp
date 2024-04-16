a2ad493cd560bb9f72008b7742a2418ebc50a08a
HyeokjuCHu <22100062@handong.ac.kr>
2024-04-16 14:42:35 +0900

    Create DS021.cpp

---
+++ DS-lab3/DS021.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+ class Student {
+    private:
+        double getAvg() const;
+    public:
+        string name;
+        string sid;
+        int *score;
+        void print() const;
+ };
+
+int main(){
+
+
+    return 0;
+}
