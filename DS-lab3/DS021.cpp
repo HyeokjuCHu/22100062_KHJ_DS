d82af092a480a7cb6463a934f4a69a89c769ed09
HyeokjuCHu <22100062@handong.ac.kr>
2024-04-16 21:37:08 +0900

    Create DS021.cpp

--- DS-lab3/DS021.cpp
+++ DS-lab3/DS021.cpp
@@ -2,18 +2,49 @@
 
 using namespace std;
 
- class Student {
-    private:
-        double getAvg() const;
-    public:
-        string name;
-        string sid;
-        int *score;
-        void print() const;
- };
+class Student
+{
+private:
+    double getAvg() const;
 
-int main(){
+public:
+    string name;
+    string sid;
+    int *score;
+    void print() const;
 
+};
+
+double Student::getAvg() const
+{
+    int add = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        add += score[i];
+    }
+
+    return static_cast<double>(add) / 3.0;
+}
+
+void Student::print() const
+{
+    cout << "[" << sid << "] " << name << endl;
+    cout.precision(1);
+    cout << "The Average score is " << fixed << getAvg();
+}
+
+int main()
+{
+    Student st;
+    st.score = new int[3];
+
+    cin >> st.sid >> st.score[0] >> st.score[1] >> st.score[2];
+    cin.ignore();
+
+    getline(cin, st.name);
+
+    st.print();
 
     return 0;
 }
