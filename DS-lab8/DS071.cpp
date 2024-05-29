4bdc5606436fe7215393d1449e18a388c964151c
HyeokjuCHu <22100062@handong.ac.kr>
2024-05-29 10:21:14 +0900

    Create DS071.cpp

--- DS-lab8/DS071.cpp
+++ DS-lab8/DS071.cpp
@@ -2,14 +2,54 @@
 
 using namespace std;
 
+int main()
+{
 
-int main(){
+    int x;
+    cin >> x;
+    int y[x];
 
+    for (int i = 0; i < x; i++)
+    {
+        cin >> y[i];
+    }
+
+    for (int i = 0; i < x; i++)
+    {
+        cout << "[" << y[i] << "] ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < x - 1; i++)
+    {
+        int min = y[i];
+        int minI;
+        int temp;
+        for (int j = i + 1; j < x; j++)
+        {
+            if (y[j] < min)
+            {
+                min = y[j];
+                minI = j;
+            }
+        }
+        temp = y[i];
+        y[i] = min;
+        y[minI] = temp;
 #ifdef _DEBUG
-    cout<<"=> Debug mode !!!"<<endl;
-#endif
+        for (int z = 0; z < x; z++)
+        {
 
-    cout<<"Hello World!!!"<<endl;
+            cout << "[" << y[z] << "] ";
+        }
+        cout << endl;
+#endif
+    }
+
+    for (int i = 0; i < x; i++)
+    {
+        cout << "[" << y[i] << "] ";
+    }
 
     return 0;
 }
