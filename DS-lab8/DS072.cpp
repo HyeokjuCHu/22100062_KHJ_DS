4523e868bf04c864e8a492ab241024334c2afa5f
HyeokjuCHu <22100062@handong.ac.kr>
2024-05-29 10:50:53 +0900

    Create DS072.cpp

---
+++ DS-lab8/DS072.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    int y[x];
+
+    for (int i = 0; i < x; i++)
+    {
+        cin >> y[i];
+    }
+#ifndef _DEBUG
+    for (int i = 0; i < x; i++)
+    {
+        cout << "[" << y[i] << "] ";
+    }
+    cout<<endl;
+#endif
+
+    for (int i = 0; i < x; i++)
+    {
+        int temp;
+        for (int j = 0; j < i + 1; j++)
+        {
+            if (y[i] < y[j])
+            {
+                temp = y[i];
+                y[i] = y[j];
+                y[j] = temp;
+            }
+        }
+#ifdef _DEBUG
+        for (int z = 0; z < x; z++)
+        {
+
+            cout << "[" << y[z] << "] ";
+        }
+        cout << endl;
+#endif
+    }
+
+#ifndef _DEBUG
+    for (int i = 0; i < x; i++)
+    {
+        cout << "[" << y[i] << "] ";
+    }
+#endif
+
+    return 0;
+}
