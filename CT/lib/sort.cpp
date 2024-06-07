aabc4b543034bd22bfeca9a1ca89dfbc5ee32698
HyeokjuCHu <22100062@handong.ac.kr>
2024-06-07 19:54:31 +0900

    Create DS101.cpp

---
+++ CT/lib/sort.cpp
@@ -0,0 +1,133 @@
+#include "sort.h"
+#include <iostream>
+
+using namespace std;
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "[" << arr[i] << "] ";
+    }
+    cout << endl;
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            swap(arr[i], arr[min]);
+        }
+#ifdef _DEBUG
+        printArray(arr, n);
+#endif
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+#ifdef _DEBUG
+        printArray(arr, n);
+#endif
+    }
+}
+
+void bubbleSortAsc(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+#ifdef _DEBUG
+        printArray(arr, n);
+#endif
+    }
+}
+
+void bubbleSortDesc(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+#ifdef _DEBUG
+        printArray(arr, n);
+#endif
+    }
+}
+
+void quickSort(int arr[], int left, int right) {
+    if (left >= right) return;
+
+    int pivot = arr[right];
+    int i = left - 1;
+
+    for (int j = left; j < right; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[right]);
+
+#ifdef _DEBUG
+    printArray(arr, right + 1);
+#endif
+
+    quickSort(arr, left, i);
+    quickSort(arr, i + 2, right);
+}
+
+void mergeArrays(int arr[], int start, int middle, int end, int totalSize) {
+    int leftPartSize = middle - start + 1;
+    int rightPartSize = end - middle;
+    int leftArray[leftPartSize];
+    int rightArray[rightPartSize];
+
+    for (int i = 0; i < leftPartSize; i++) {
+        leftArray[i] = arr[start + i];
+    }
+    for (int i = 0; i < rightPartSize; i++) {
+        rightArray[i] = arr[middle + i + 1];
+    }
+    
+    int i = 0, j = 0, k = start;
+
+    while (i < leftPartSize && j < rightPartSize) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k++] = leftArray[i++];
+        } else {
+            arr[k++] = rightArray[j++];
+        }
+    }
+    while (i < leftPartSize) {
+        arr[k++] = leftArray[i++];
+    }
+    while (j < rightPartSize) {
+        arr[k++] = rightArray[j++];
+    }
+
+#ifdef _DEBUG
+    printArray(arr, totalSize);
+#endif
+}
+
+void mergeSort(int arr[], int start, int end, int totalSize) {
+    if (start >= end) return;
+    int middle = start + (end - start) / 2;
+    mergeSort(arr, start, middle, totalSize);
+    mergeSort(arr, middle + 1, end, totalSize);
+    mergeArrays(arr, start, middle, end, totalSize);
+}
