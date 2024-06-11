bb2d5bfa2a102fe969edfe28e3cb0ed80db5432f
HyeokjuCHu <22100062@handong.ac.kr>
2024-06-11 15:18:43 +0900

    Create MinHeapQ

--- DS-lab10/DS091.cpp
+++ DS-lab10/DS091.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
-int main(){
-    vector <int> a;
-    
-    int x;
-    cin>>x;
+int main()
+{
+    int arr[5]={1,5,4,3,6};
+    priority_queue<int> x;
 
-    a.push_back(x);
-    a.pop_back();
-    cout<<a[0];
+    cout<<"Init: ";
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
     }
+    cout<<endl;
+
+    for(int i=4;i>=0;i--){
+        x.push(arr[i]);
+    }
+
+    cout<<"Priority Queue: ";
+    while(!x.empty()){
+        cout<<x.top()<<' ';
+        x.pop();
+    }
+    for(int i=0;i<5;i++){
+        cout<<endl;
+    }
+}
